c8011df37b500d8e32bf28ed3fa12caa85fc2991
Laurent Chouraki <laurent@MacBook-Air-de-Laurent.local>
2026-02-24 14:24:39 +0100

    premiere API flask

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+# Import libs
+from flask import Flask, render_template, request
+
+# create application instance
+app = Flask(__name__)
+
+# create route
+@app.route('/')
+def home():
+    return "Welcome to the Alyra deployment API!"
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return {"status": "healthy", "message": "The API is up and running!"}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
